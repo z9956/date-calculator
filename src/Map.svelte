c6886f84d5498317a20bea877a04a1bca1c1a979
z9956 <zhz995617@gmail.com>
2025-02-07 15:22:18 +0800

    commit

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,5 +1,5 @@
 <script>
-    const maps = [  "奥林匹斯", "世界尽头","电力区",];
+    const maps = ["世界尽头", "电力区", "奥林匹斯",];
 
     function getMap() {
         const today = new Date();
@@ -9,11 +9,36 @@
             dayAfterTomorrowMap: maps[(index + 2) % maps.length], };
     }
 
+    const isLeapYear = (year) => {
+        return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    };
+
+    function getProgress() {
+        // 获取当前日期
+        const currentDate = new Date();
+
+        // 获取当前年份和对应的天数
+        const year = currentDate.getFullYear();
+        const daysInYear = isLeapYear(year) ? 366 : 365;
+
+        // 获取当前日期是今年的第几天
+        const startOfYear = new Date(year, 0, 0);
+        const diff = currentDate - startOfYear;
+        const oneDay = 1000 * 60 * 60 * 24;
+        const dayOfYear = Math.floor(diff / oneDay);
+
+        return (dayOfYear / daysInYear) * 100;
+    }
+
     const {currentMap, tomorrowMap, dayAfterTomorrowMap} = getMap();
+    const progressPercentage = getProgress()
 </script>
 
 <main>
     <div class="map-container">
+        <div class="progress-bar">
+            <div class="progress" title="{Math.round(progressPercentage)}%" style="width: {progressPercentage}%"></div>
+        </div>
         今天：<span>{currentMap}</span> |
         明天：<span>{tomorrowMap}</span> |
         后天：<span>{dayAfterTomorrowMap}</span>
@@ -30,5 +55,22 @@
         border: 1px solid #ccc;
         border-radius: 8px;
         text-align: center;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .progress-bar {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        position: absolute;
+        top: 0;
+        left: 0;
+
+    }
+
+    .progress {
+        height: 100%;
+        background-color: rgba(121, 134, 203, 0.3);
     }
 </style>
